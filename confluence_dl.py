bc1ec1ba5dc27c277ca4fea88626f0ae63235ca5
Wes Moskal-Fitzpatrick <github@fitzmoskal.me>
2023-10-02 19:27:06 +0100

    Update to confluence_dl.py
    - Added an option to specify a depth of 1-5
    - Added method to use auth token to get around missing page-id for some pages
    - Can now specify location of config.yaml and save directory

--- confluence_dl.py
+++ confluence_dl.py
@@ -1,117 +1,105 @@
-# Author Wes Moskal-Fitzpatrick
+# Author: Wes Moskal-Fitzpatrick
 #
 # Download from Confluence as PDFs
 #
 
 import requests
-from bs4 import BeautifulSoup
+import json
 import argparse
-from urllib.parse import urlsplit
+import re
 import yaml
-import re  # Importing Regular Expression library to sanitize filenames
+import os
+from bs4 import BeautifulSoup
+from urllib.parse import urlsplit
 
 def read_config(file_path):
     with open(file_path, 'r') as f:
         return yaml.safe_load(f)
 
-config = read_config("config.yaml")
-USERNAME = config['username']
-PASSWORD = config['password']
-BASE_URL = config['confluence_server']
-LOGIN_URL = BASE_URL + "/dologin.action"
-EXPORT_URL = BASE_URL + "/spaces/flyingpdf/pdfpageexport.action"
-
 def sanitize_url(url):
-    # Removing newlines, spaces, and tabs
-    url = url.strip().replace("\n", "").replace("\t", "")
-    
-    # Further sanitize the URL by unquoting it, which will convert %20 to spaces, etc.
-    from urllib.parse import unquote
-    return unquote(url)
+    return url.strip().replace("\n", "").replace("\t", "")
 
 def sanitize_filename(filename):
     return re.sub(r'[\/:*?"<>|]', '', filename)
 
-def download_page_as_pdf(session, page_url):
+def get_page_id_from_api(session, page_url):
+    page_title = page_url.split('/')[-1]
+    api_url = f"{BASE_URL}/rest/api/content?title={page_title}"
+    response = session.get(api_url)
+    if response.ok:
+        data = json.loads(response.text)
+        if 'results' in data and len(data['results']) > 0:
+            return data['results'][0]['id']
+    return None
 
-    # Check if the URL is relative and make it absolute
-    if not page_url.startswith('http'):
-        page_url = BASE_URL + page_url
-        page_url = sanitize_url(page_url)
+def download_page_as_pdf(session, page_url, save_location, visited_pages):
+    if page_url in visited_pages:
+        print(f"Page {page_url} already downloaded. Skipping...")
+        return []
 
-    # Split the URL
-    parsed_url = urlsplit(page_url)
-    netloc = parsed_url.netloc
+    if page_url.startswith("/"):
+        full_url = BASE_URL + page_url
+    else:
+        full_url = page_url
+    parsed_url = urlsplit(full_url)
+    if 'confluence' not in parsed_url.netloc:
+        print(f"Skipping external link: {full_url}")
+        return []
 
-    # Check if netloc contains spaces or @ sign
-    if ' ' in netloc or '@' in netloc:
-        print(f"Skipping malformed URL: {page_url}")
-        return
+    response = session.get(full_url)
 
-    # Navigate to the actual page first
-    response = session.get(page_url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Extract pageId from the soup, if it exists
-    page_id_elements = soup.find_all('meta', {'name': 'ajs-page-id'})
-    if page_id_elements:
-        page_id = page_id_elements[0]['content']
+    match = re.search(r'pageId=(\d+)', page_url)
+    if match:
+        page_id = match.group(1)
     else:
+        page_id = get_page_id_from_api(session, page_url)
+
+    if not page_id:
         print(f"Couldn't determine pageId for {page_url}. Skipping...")
-        return
-    
-    # Download the current page as PDF
-    page_title = sanitize_filename(soup.title.string)  # Getting the page title
-    pdf_export_url = EXPORT_URL + "?pageId=" + page_id
+        return []
+
+    page_title = sanitize_filename(soup.title.string)
+    pdf_export_url = f"{BASE_URL}/spaces/flyingpdf/pdfpageexport.action?pageId={page_id}"
     pdf_response = session.get(pdf_export_url, stream=True)
 
-    # Save the PDF
-    filename = f"{page_title}.pdf"  # Using the title for filename
-    #filename = page_id + '.pdf'
+    filename = os.path.join(save_location, f"{page_title}.pdf")
     with open(filename, 'wb') as f:
         for chunk in pdf_response.iter_content(chunk_size=1024):
             if chunk:
                 f.write(chunk)
 
     print(f"Downloaded {page_url} as {filename}")
-
-    # Return links from this page
-    return [a['href'] for a in soup.find_all('a', href=True) if 'viewpage.action?pageId=' in a['href'] or '/display/' in a['href']]
-
-def main(page_url, depth):
-
-    session = requests.Session()
-
-    login_data = {
-        'os_username': USERNAME,
-        'os_password': PASSWORD
-    }
-    response = session.post(LOGIN_URL, data=login_data)
-
-    if response.ok:
-        print("Logged in successfully!")
-    else:
-        print("Failed to log in.")
-        exit()
-
-    # Maintain a set of pages we've visited to avoid duplication
-    visited_pages = set()
-
-    # Download the main page and get its links
-    links_on_main_page = download_page_as_pdf(session, page_url)
     visited_pages.add(page_url)
 
-    # If depth is 1, download directly linked pages
-    if depth == 1:
-        for link in links_on_main_page:
-            if link not in visited_pages:
-                download_page_as_pdf(session, link)
-                visited_pages.add(link)
+    return [a['href'] for a in soup.find_all('a', href=True) if 'viewpage.action?pageId=' in a['href'] or '/display/' in a['href']]
+
+def main(page_url, depth, config_path, save_location):
+    config = read_config(config_path)
+    TOKEN = config['cf_token']
+    global BASE_URL
+    BASE_URL = config['confluence_server']
+
+    session = requests.Session()
+    session.headers.update({"Authorization": f"Bearer {TOKEN}"})
+
+    visited_pages = set()
+    queue = [(page_url, 0)]
+
+    while queue:
+        url, current_depth = queue.pop(0)
+        if current_depth > depth:
+            continue
+        new_links = download_page_as_pdf(session, url, save_location, visited_pages)
+        queue.extend((link, current_depth + 1) for link in new_links)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Download a Confluence page and its directly linked pages as PDFs based on depth.")
+    parser = argparse.ArgumentParser(description="Download Confluence pages as PDFs based on depth.")
     parser.add_argument("page_url", help="The page URL")
-    parser.add_argument("--depth", type=int, default=0, choices=[0, 1], help="Depth of downloading: 0 for only main page, 1 for main page and directly linked pages.")
+    parser.add_argument("--depth", type=int, default=1, choices=[1, 2, 3, 4, 5], help="Depth of downloading: 1 for main page, up to 5 for nested pages.")
+    parser.add_argument("--config", default="config.yaml", help="Path to the configuration file.")
+    parser.add_argument("--save", default="./", help="Location to save PDF files.")
     args = parser.parse_args()
 
-    main(args.page_url, args.depth)
+    main(args.page_url, args.depth, args.config, args.save)
--- docs/confluence_dl.md
+++ docs/confluence_dl.md
@@ -1,16 +1,26 @@
 # Confluence PDF Downloader
 
 ## Description
+This script downloads Confluence pages as PDFs. It starts with a specified page and can go down to nested pages up to a certain depth. It uses a YAML configuration file to read the API token and server information.
 
-This Python script allows you to download pages from a Confluence site as PDF files. The saved PDFs will be named after the title of the Confluence page.
+## Author
+Wes Moskal-Fitzpatrick
 
 ## Dependencies
-
 - Python 3.x
 - `requests`
-- `beautifulsoup4`
-- `PyYAML`
-- `re` (Regular Expression)
+- `BeautifulSoup` from `bs4`
+- `argparse`
+- `re`
+- `yaml`
+- `os`
+- `json`
+- `urllib.parse`
+
+## Installation
+```bash
+pip install requests BeautifulSoup4 PyYAML
+```
 
 ## Configuration
 
@@ -19,6 +29,7 @@ Create a `config.yaml` file in the same directory as the script. The file should
 ```yaml
 username: YourConfluenceUsername
 password: YourConfluencePassword
+cf_token: YOUR_CONFLUENCE_API_TOKEN
 confluence_server: https://YourConfluenceServer.com
 ```
 
@@ -29,20 +40,23 @@ Replace the placeholders with your Confluence username, password, and server URL
 1. Open terminal and navigate to the directory containing the script.
 2. Run the script using the following command:
 
-   ```
-   python confluence_ld.py [page_url] [--depth DEPTH]
-   ```
+```bash
+python confluence_dl.py <page_url> [--depth DEPTH] [--config CONFIG_PATH] [--save SAVE_LOCATION]
+```
 
-   - `page_url`: The URL of the Confluence page you want to download.
-   - `--depth`: Optional. 0 for the main page only, 1 for the main page and directly linked pages. Default is 0.
+- `page_url`: URL of the Confluence page to start downloading from.
+- `--depth`: Depth of downloading. Starts with the main page and goes up to 5 levels deep. (default is 1)
+- `--config`: Path to the YAML configuration file. (default is `config.yaml`)
+- `--save`: Directory to save the downloaded PDFs. (default is current directory)
 
-## Functions
+### Example
+```bash
+python confluence_dl.py https://your_confluence_server/display/YOUR_PAGE --depth 3 --config config.yaml --save ./pdfs
+```
 
-- `sanitize_url(url)`: Sanitizes the given URL.
-- `sanitize_filename(filename)`: Sanitizes the filename.
-- `read_config(file_path)`: Reads configurations from a YAML file.
-- `main(page_url, depth)`: Main function to download the page(s).
+## Warning
+When using a high `--depth` number, the script may end up downloading a large number of documents, consuming bandwidth and disk space. Exercise caution and ensure adequate resources are available.
 
----
-
-This README provides an overview of what the script does, its dependencies, configuration, and how to use it. Feel free to adjust as needed.
+## Troubleshooting
+- Make sure you have a valid Confluence API token and server URL in your `config.yaml`.
+- If you get an "Unauthorized" error, verify that the API token is correct.
